ca67081dda860e784779cd26448b0fc30d916eff
David Moore <davmre@gmail.com>
2014-01-22 22:54:53 -0800

    added build instructions to README

--- Matrizer.hs
+++ Matrizer.hs
@@ -9,20 +9,7 @@ import MTypes
 import Parsing
 import Optimization
 import Analysis
-
-
--------------------------------------------------------------
-
--- Code generation for numpy arrays
-
-generateNumpy :: MTree -> String
-generateNumpy (Leaf a) = [a]
-generateNumpy (Branch2 MProduct t1 t2) = "np.dot(" ++ (generateNumpy t1) ++ 
-                                          ", " ++ (generateNumpy t2)  ++ ")" 
-generateNumpy (Branch2 MSum t1 t2) = (generateNumpy t1) ++ " + " ++ (generateNumpy t2)
-generateNumpy (Branch1 MInverse t) = "np.linalg.inv(" ++ (generateNumpy t) ++ ")"
-generateNumpy (Branch1 MTranspose t) = (generateNumpy t) ++ ".T" -- caution: might we need parentheses here?
-generateNumpy (Branch1 MNegate t) = "-" ++ (generateNumpy t)
+import CodeGen
 
 ---------------------------------------------------------------
 
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 matrizer
 ========
 
-Optimizing compiler for matrix expressions, targeting numpy / MATLAB
+Optimizing compiler for matrix expressions, targeting numpy / MATLAB.
+
+To build: runhaskell Setup.hs build
+
+Try an example: dist/build/matrizer/matrizer examples/simple.mtr
