6b2c7228c69b6eadc65f12244d16c581ecaf0894
David Moore <davmre@gmail.com>
2015-04-14 20:48:03 -0700

    special-case adding a negative

--- src/CodeGen.hs
+++ src/CodeGen.hs
@@ -18,6 +18,7 @@ generateNumpy (Branch2 MCholSolve t1 t2) = "scipy.linalg.cho_solve(scipy.linalg.
                                           "), " ++ (generateNumpy t2)  ++ ")" 
 generateNumpy (Branch2 MProduct t1 t2) = "np.dot(" ++ (generateNumpy t1) ++ 
                                           ", " ++ (generateNumpy t2)  ++ ")" 
+generateNumpy (Branch2 MSum t1 (Branch1 MNegate t2)) = (generateNumpy t1) ++ " - " ++ (generateNumpy t2)
 generateNumpy (Branch2 MSum t1 t2) = (generateNumpy t1) ++ " + " ++ (generateNumpy t2)
 generateNumpy (Branch1 MInverse t) = "np.linalg.inv(" ++ (generateNumpy t) ++ ")"
 generateNumpy (Branch1 MTranspose t) = (generateNumpy t) ++ ".T" -- caution: might we need parentheses here?
