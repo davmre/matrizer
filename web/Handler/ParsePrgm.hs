146d31827e5584be7ab91a1ca9c2adc8117a4944
Dave Moore <dmoore@cs.berkeley.edu>
2015-05-17 23:27:12 -0700

    log submitted programs

--- web/Handler/ParsePrgm.hs
+++ web/Handler/ParsePrgm.hs
@@ -8,6 +8,7 @@ import Yesod.Core.Json
 postParsePrgmR :: Handler Value
 postParsePrgmR = 
           do postedText <- runInputPost $ ireq textField "program"
+	     $(logOther "Submission") $ "processing text: \n" ++ postedText ++ "\n"
              case doParse (unpack postedText) of
                   Left err -> sendResponseStatus status400 $ object [("error", String $ pack $ show err)]
                   Right (tbl, tree, flops) -> return $ object [("symboltable", String $ pack $ show tbl), ("prgm", String $ pack $ show tree), ("flops", Number $ fromIntegral flops)]
