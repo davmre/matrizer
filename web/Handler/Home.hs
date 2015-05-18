a93531a64f58ed5284ad7c872cda679e58001d49
David Moore <davmre@gmail.com>
2015-05-17 18:16:06 -0700

    ajax loading, no error handling yet

--- web/Handler/Home.hs
+++ web/Handler/Home.hs
@@ -4,26 +4,8 @@ import Import
 import Matrizer.Util
 
 getHomeR :: Handler Html
-getHomeR = defaultLayout (setTitle "Matrizer" >> $(widgetFile "main"))
+getHomeR = defaultLayout $ do
+         setTitle "Matrizer" 
+         addScriptRemote "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"
+         $(widgetFile "main")
 
-postHomeR :: Handler Html
-postHomeR = do
-              postedText <- runInputPost $ ireq textField "program"
-              defaultLayout (setTitle "Optimized" >> [whamlet|
-<table border=0>
-  <tr>
-    <td valign=top>
-      <p>Matrizer input:
-      <form method=post action=@{HomeR}>
-        <textarea cols=40 rows=20  name=program>#{postedText}
-        <input type=submit value="Optimize">
-    <td width=10% cellpadding=20>
-    <td valign=top>
-      <p>Matrizer output: 
-      <pre>#{optimizeRawText postedText}
-<p>
-<p>
-|])
-
-optimizeRawText :: Text -> Text
-optimizeRawText t = pack $ optimizeStr $ unpack t
