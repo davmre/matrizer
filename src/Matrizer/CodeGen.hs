9437337e2805f1eee8f3fed81d8b55a52193e155
David Moore <davmre@gmail.com>
2015-05-24 22:53:59 -0700

    expose optimizer options in web interface, subtly

--- src/Matrizer/CodeGen.hs
+++ src/Matrizer/CodeGen.hs
@@ -109,6 +109,9 @@ generateMatlab (Branch2 MTriSolve t1 t2) = "(" ++ (generateMatlab t1) ++
                                           "\\" ++ (generateMatlab t2)  ++ ")"
 generateMatlab (Branch2 MCholSolve (Branch1 MChol t1) t2) = "(" ++ (generateMatlab t1) ++
                                             ")\\" ++ (generateMatlab t2) ++ ")"
+generateMatlab (Branch2 MCholSolve t1 t2) = "(" ++ (generateMatlab t1) ++
+                                            "'\\(" ++ (generateMatlab t1) ++
+                                            "\\" ++ (generateMatlab t2) ++ "))"
 generateMatlab (Branch2 MProduct t1 t2) = "(" ++ (generateMatlab t1) ++
                                           " * " ++ (generateMatlab t2)  ++ ")"
 generateMatlab (Branch2 MScalarProduct t1 t2) = "(" ++ (generateMatlab t1) ++
